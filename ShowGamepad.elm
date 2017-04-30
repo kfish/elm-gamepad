74a92cb34d941a72a2d0488824a3bfd745da298b
Conrad Parker <conrad@metadecks.org>
2017-05-01 07:34:58 +0800

    ShowGamepads.elm: Update for elm 0.18

--- ShowGamepad.elm
+++ ShowGamepad.elm
@@ -1,9 +1,43 @@
-import Color exposing (..)
-import Graphics.Collage exposing (..)
-import Graphics.Element exposing (..)
-import Graphics.Element exposing (..)
+module Main exposing (..)
+
+import Html exposing (text)
+
 import Gamepad
 
-main : Signal Element
+import Html exposing (Html, div, text, program)
+
+type Msg
+    = GamepadMsg (List Gamepad.Gamepad)
+
+type alias Model =
+    List Gamepad.Gamepad
+
+init : ( Model, Cmd Msg )
+init =
+    ( [], Gamepad.gamepads GamepadMsg )
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ text (toString model) ]
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GamepadMsg gamepads ->
+            ( gamepads, Gamepad.gamepads GamepadMsg )
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.batch
+        [
+        ]
+
+main : Program Never Model Msg
 main =
-  Signal.map show Gamepad.gamepads
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
