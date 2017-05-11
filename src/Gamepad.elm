03699c51fcdf884d86e8a9f6acbdd9b500c0d4b2
Conrad Parker <conrad@metadecks.org>
2017-05-12 05:55:59 +0800

    Indentation

--- src/Gamepad.elm
+++ src/Gamepad.elm
@@ -36,10 +36,13 @@ type alias Button  = Types.Button
 {-| Gamepad -}
 type alias Gamepad = Types.Gamepad
 
+
 {-| Get the currently connected gamepads
 -}
 gamepads : (List Gamepad -> msg) -> Cmd msg
-gamepads tagger = Task.map (List.map convert) getRawGamepads |> Task.perform tagger
+gamepads tagger =
+    Task.map (List.map convert) getRawGamepads
+    |> Task.perform tagger
 
 getRawGamepads : Task x (List Types.RawGamepad_)
 getRawGamepads = Native.Gamepad.gamepads 1.0
--- src/Internal/Types.elm
+++ src/Internal/Types.elm
@@ -2,58 +2,58 @@ module Internal.Types exposing (..)
 
 {-| Button -}
 type alias Button =
-  { pressed : Bool
-  , value : Float
-  }
+    { pressed : Bool
+    , value : Float
+    }
 
-{-| Axis -}
+{-| Axes -}
 type alias Axes =
-  { x : Float
-  , y : Float
-  }
+    { x : Float
+    , y : Float
+    }
 
 {-| Gamepad -}
 type Gamepad =
-  StandardGamepad StandardGamepad_
-  | RawGamepad RawGamepad_
+      StandardGamepad StandardGamepad_
+    | RawGamepad RawGamepad_
 
 -- | StandardGamepad
 type alias StandardGamepad_ =
-  { id : String
+    { id : String
 
-  , buttonBack   : Button
-  , buttonStart  : Button
-  , buttonLogo   : Button
+   , buttonBack   : Button
+   , buttonStart  : Button
+   , buttonLogo   : Button
 
-  , buttonA : Button
-  , buttonB : Button
-  , buttonX : Button
-  , buttonY : Button
+   , buttonA : Button
+   , buttonB : Button
+   , buttonX : Button
+   , buttonY : Button
 
-  , leftTrigger    : Button
-  , leftBumper     : Button
-  , leftStick      : Axes
-  , leftStickPress : Button
+   , leftTrigger    : Button
+   , leftBumper     : Button
+   , leftStick      : Axes
+   , leftStickPress : Button
 
-  , rightTrigger    : Button
-  , rightBumper     : Button
-  , rightStick      : Axes
-  , rightStickPress : Button
+   , rightTrigger    : Button
+   , rightBumper     : Button
+   , rightStick      : Axes
+   , rightStickPress : Button
 
-  , dPadUp    : Button
-  , dPadDown  : Button
-  , dPadLeft  : Button
-  , dPadRight : Button
-  }
+   , dPadUp    : Button
+   , dPadDown  : Button
+   , dPadLeft  : Button
+   , dPadRight : Button
+   }
 
 {-| RawGamepad -}
 type alias RawGamepad_ =
-  { id : String
-  , axes : List Float
-  , buttons : List Button
-  , mapping : String
-  -- connected
-  -- index
-  -- timestamp
-  }
+   { id : String
+   , axes : List Float
+   , buttons : List Button
+   , mapping : String
+   -- connected
+   -- index
+   -- timestamp
+   }
 
